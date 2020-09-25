fe9b4050a1a9ad13669244ad43b81eb5b119696d
Joshua Malomo <rexunborn@gmail.com>
2020-09-25 12:32:56 +0100

    Finished basic mastermind

--- mastermind.rb
+++ mastermind.rb
@@ -1,8 +1,9 @@
 class Game
   attr_reader :random_code
   def initialize
-    @code = %w[red blue yellow black]
-    @random_code = @code.shuffle
+    @colors = %w[r o y g b i]
+    @random_code = @colors.shuffle
+    @com_code = @random_code.slice(0, 4)
   end
 end
 
@@ -15,27 +16,42 @@ class Player < Game
 
   def guess(str)
     @array = str.split(' ')
-    self.check_code
-    @turns -= 1
-    if @turns.zero?
-      @turns = 12
-      'Game Over!'
+    if @array == @com_code
+      puts "You guessed the code: #{@com_code}"
+      reset
+    else
+      check_code
     end
+    @turns -= 1
+    turn_check
   end
 
   def check_code
     i = 0
     while i < 4
-
-      if @array == @random_code
-        return "You guessed the code: #{@random_code}"
-      elsif @array[i] == @random_code[i]
-        puts "#{array[i]} is correct"
+      if @com_code.include?(@array[i])
+        if @array[i] == @com_code[i]
+          puts "#{@array[i]} is correct and well placed"
+        else
+          puts "#{@array[i]} is correct but placed wrong"
+        end
       else
-        puts "#{array[i]} is incorrect"
+        puts "#{@array[i]} is incorrect"
       end
-
       i += 1
     end
+  end
+
+  def turn_check
+    if @turns.zero?
+      reset
+      'Game Over'
+    end
+  end
+
+  def reset
+    @turns = 12
+    @random_code = @colors.shuffle
+    @com_code = @random_code.slice(0, 4)
   end
 end
